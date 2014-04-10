7989adc8e6e2293aca8909813cb87644b9a030b9
L22A2 <hk.l85a2@gmail.com>
2014-04-10 16:32:23 +0200

    init

---
+++ GodHand/GodHand/GodHandProject/MyScene.cs
@@ -0,0 +1,20 @@
+#region Using Statements
+using System;
+using WaveEngine.Common;
+using WaveEngine.Common.Graphics;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Services;
+#endregion
+
+namespace GodHandProject
+{
+    public class MyScene : Scene
+    {
+        protected override void CreateScene()
+        {
+            RenderManager.BackgroundColor = Color.CornflowerBlue;
+
+            //Insert your code here
+        }
+    }
+}
